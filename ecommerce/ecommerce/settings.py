5c8c58712f5eab46e60a8330e89f4a03b837a217
amir <137843605+amirizo@users.noreply.github.com>
2024-09-08 21:49:04 +0000

    Update settings.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOSt = 'stmp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'amirmussa@gmail.com'
-EMAIL_HOST_PASSWORD = 'kijangwani'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
